88f810d3be7b1bf81016946bda848fb016c69b72
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-22 19:53:16 -0700

    01_03 Building a web server - html

--- Ch01/01_03/start/index.js
+++ Ch01/01_03/start/index.js
@@ -4,9 +4,18 @@ const { createServer } = require("http");
 
 createServer((req, res) => {
   res.writeHead(200, {
-    "Content-Type": "text/plain",
+    "Content-Type": "text/html",
   });
-  res.end("Hello World");
+  console.log(req);
+  res.end(`
+  <!DOCTYPE html>
+  <html>
+    <body>
+      <h1>Serving HTML Text</h1>
+      <p>${req.method} request made for ${req.url}</p>
+    </body>
+  </html>
+  `);
 }).listen(3000);
 
 console.log("web server is listening on 3000");
