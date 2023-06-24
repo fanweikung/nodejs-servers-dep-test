be69659097f01bc6a7d73437c1ff421366da4ce3
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-24 06:39:04 -0700

    Added app.post with body-parser

--- Ch03/03_03/start/server.js
+++ Ch03/03_03/start/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bodyparser from "body-parser";
+import fs from "fs";
 import skiTerms from "./ski-terms.json" assert { type: "json" };
 
 const app = express();
@@ -8,6 +10,13 @@ app.get("/dictionary", (req, res) => {
   res.json(skiTerms);
 });
 
-app.listen(3000, () =>
-  console.log("ski dictionary running at 3000")
-);
+app.post("/dictionary", bodyparser.json(), (req, res) => {
+  console.log(req.body);
+  skiTerms.push(req.body);
+  res.json({
+    status: "success",
+    term: req.body,
+  });
+});
+
+app.listen(3000, () => console.log("ski dictionary running at 3000"));
