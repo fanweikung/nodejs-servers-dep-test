e70b3d63dab2dd1654c9382220fd43dcace1227b
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-06-26 06:11:41 -0700

    05_04 Using code coverage finished start/

--- Ch05/05_04/start/src/app.test.js
+++ Ch05/05_04/start/src/app.test.js
@@ -4,5 +4,6 @@ import app from "./app";
 describe("API Tests", () => {
   it("GET - /dictionary", async () => {
     const { body } = await request(app).get("/dictionary");
+    expect(body.length).toEqual(3);
   });
 });
